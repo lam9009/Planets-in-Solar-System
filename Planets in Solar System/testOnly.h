517b2cb3a801d54f30b28e728af73dbe42dc8c8d
lam9009 <lam9009@yahoo.com>
2015-01-04 13:43:40 -0800

    add my testOnly file

---
+++ Planets in Solar System/testOnly.h
@@ -0,0 +1,13 @@
+//
+//  testOnly.h
+//  Planets in Solar System
+//
+//  Created by Alfred on 4/1/15.
+//  Copyright (c) 2015 maptier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testOnly : NSObject
+
+@end
---
+++ Planets in Solar System/testOnly.m
@@ -0,0 +1,13 @@
+//
+//  testOnly.m
+//  Planets in Solar System
+//
+//  Created by Alfred on 4/1/15.
+//  Copyright (c) 2015 maptier. All rights reserved.
+//
+
+#import "testOnly.h"
+
+@implementation testOnly
+
+@end
